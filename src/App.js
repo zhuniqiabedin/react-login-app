a05658a3a83b1e71b48bfdb71866afa374358399
Abedin Zhuniqi <>
2019-10-17 23:05:29 +0200

    Added the UI for Login, Register.

--- src/App.js
+++ src/App.js
@@ -1,26 +1,34 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import LoginScreen from "./routes/LoginScreen";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      loginPage: [],
+      uploadScreen: []
+    }
+  }
+
+  componentDidMount() {
+    const loginPage = [];
+    loginPage.push(<LoginScreen parentContext={this}/>);
+    this.setState({
+      loginPage:loginPage
+    })
+  }
+  render() {
+    return(
+        <div className="App">
+          {this.state.loginPage}
+          {this.state.uploadScreen}
+        </div>
+    )
+  }
 }
+const style = {
+  margin: 15,
+};
 
 export default App;
